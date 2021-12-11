84e37a1202a062c6d926c7c6d4f423e986df3eec
obinna asiegbulam <oasiegbulam@gmail.com>
2021-12-11 22:09:21 +0100

    JWT settings class added

---
+++ SignalRAPI.Core/Implementations/AuthenticationService.cs
@@ -0,0 +1,249 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using Serilog;
+using SignalRAPI.Core.Interfaces;
+using SignalRAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalRAPI.Core.Implementations
+{
+    public class AuthenticationService : IAuthenticationService
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly IMapper _mapper;
+        private readonly ILogger _logger;
+        private readonly JWTSettings _jwtSettings;
+
+        public AuthenticationService(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager,
+            IMapper mapper, ILogger logger, IOptions<JWTSettings> jwtSettings)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _mapper = mapper;
+            _logger = logger;
+            _jwtSettings = jwtSettings.Value;
+        }
+
+        public async Task<Response<LoginResponse>> Login(LoginRequest model, string ipAddress)
+        {
+            _logger.Information("Login Attempt");
+            var user = await _userManager.FindByEmailAsync(model.Email);
+            var validityResult = await ValidateUser(model);
+            if (!validityResult.Succeeded)
+            {
+                _logger.Error("Login operation failed");
+                return Response<LoginResponse>.Fail(validityResult.Message);
+            }
+            var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
+
+            if (!result.Succeeded)
+            {
+                _logger.Error("Login operation failed");
+                return Response<LoginResponse>.Fail($"Invalid Credentials for '{model.Email}'.");
+            }
+
+            JwtSecurityToken jwtSecurityToken = await GenerateJWToken(user);
+            var loginResponse = new LoginResponse()
+            {
+                Id = user.Id,
+                JWToken = new JwtSecurityTokenHandler().WriteToken(jwtSecurityToken),
+                RefreshToken = GenerateRefreshToken(ipAddress).Token
+            };
+            return Response<LoginResponse>.Success($"{user.UserName} logged in succesfully", loginResponse);
+        }
+
+        public async Task<Response<string>> Register(RegisterRequest model, string origin)
+        {
+            var user = _mapper.Map<AppUser>(model);
+            user.UserName = model.Email;
+
+            using var transaction = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled);
+            var result = await _userManager.CreateAsync(user, model.Password);
+            if (result.Succeeded)
+            {
+                _logger.Information($"{model.Email} created successfully");
+                await _userManager.AddToRoleAsync(user, Roles.Regular.ToString());
+
+                var emailResult = await SendVerificationEmail(user, origin);
+                if (emailResult)
+                {
+                    _logger.Information("Mail sent succesfully");
+                    transaction.Complete();
+                    return Response<string>.Success(user.Id, "User created successfully! Please check your mail to verify your account.");
+                }
+
+                _logger.Information("Mail service failed");
+                transaction.Dispose();
+                return Response<string>.Fail("Registration failed. Please try again.");
+            }
+
+            transaction.Complete();
+            return Response<string>.Fail(GetErrors(result));
+
+        }
+
+        public async Task<Response<string>> ConfirmEmailAsync(string userId, string code)
+        {
+            var user = await _userManager.FindByIdAsync(userId);
+            code = Encoding.UTF8.GetString(WebEncoders.Base64UrlDecode(code));
+            var result = await _userManager.ConfirmEmailAsync(user, code);
+            if (result.Succeeded)
+            {
+                return Response<string>.Success(user.Id, $"Account Confirmed for {user.Email}. You can now use the /api/Account/authenticate endpoint.");
+            }
+
+            return Response<string>.Fail($"An error occured while confirming {user.Email}.");
+
+        }
+
+        public async Task ForgotPassword(ForgotPasswordRequest model, string origin)
+        {
+            var account = await _userManager.FindByEmailAsync(model.Email);
+
+            // always return ok response to prevent email enumeration
+            if (account == null) return;
+
+            var code = await _userManager.GeneratePasswordResetTokenAsync(account);
+            var route = "api/account/reset-password/";
+            var _enpointUri = new Uri(string.Concat($"{origin}/", route));
+            var emailRequest = new EmailRequest()
+            {
+                Body = $"You reset token is - {code}",
+                ToEmail = model.Email,
+                Subject = "Reset Password",
+            };
+            await _emailService.SendEmailAsync(emailRequest);
+        }
+
+        public async Task<Response<string>> ResetPassword(ResetPasswordRequest model)
+        {
+            var account = await _userManager.FindByEmailAsync(model.Email);
+            if (account == null) return Response<string>.Fail($"No Accounts Registered with {model.Email}.");
+            var result = await _userManager.ResetPasswordAsync(account, model.Token, model.Password);
+            if (result.Succeeded)
+            {
+                return Response<string>.Success(model.Email, $"Your password reset was successful.");
+            }
+            return Response<string>.Fail($"Error occured while reseting the password.");
+
+        }
+
+
+
+        //PRIVATE METHODS
+        private static string GetErrors(IdentityResult result)
+        {
+            return result.Errors.Aggregate(string.Empty, (current, err) => current + err.Description + "\n");
+        }
+
+        /// <summary>
+        /// Validates a user
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns>Returns true if the user exists</returns>
+        private async Task<Response<bool>> ValidateUser(LoginRequest model)
+        {
+            var user = await _userManager.FindByEmailAsync(model.Email);
+            var response = new Response<bool>();
+            if (user == null || !await _userManager.CheckPasswordAsync(user, model.Password))
+            {
+                response.Message = "Invalid Credentials";
+                response.Succeeded = false;
+                response.StatusCode = (int)HttpStatusCode.BadRequest;
+                return response;
+            }
+            if (!await _userManager.IsEmailConfirmedAsync(user))
+            {
+                response.Message = "Account not activated";
+                response.Succeeded = false;
+                response.StatusCode = (int)HttpStatusCode.Forbidden;
+                return response;
+            }
+            else
+            {
+                response.Succeeded = true;
+                return response;
+            }
+        }
+        private RefreshToken GenerateRefreshToken(string ipAddress)
+        {
+            return new RefreshToken
+            {
+                Token = RandomTokenString(),
+                Expires = DateTime.UtcNow.AddDays(7),
+                Created = DateTime.UtcNow,
+                CreatedByIp = ipAddress
+            };
+        }
+        private string RandomTokenString()
+        {
+            using var rngCryptoServiceProvider = new RNGCryptoServiceProvider();
+            var randomBytes = new byte[40];
+            rngCryptoServiceProvider.GetBytes(randomBytes);
+            // convert random bytes to hex string
+            return BitConverter.ToString(randomBytes).Replace("-", "");
+        }
+        private async Task<JwtSecurityToken> GenerateJWToken(AppUser user)
+        {
+            var userClaims = await _userManager.GetClaimsAsync(user);
+            var roles = await _userManager.GetRolesAsync(user);
+
+            var roleClaims = new List<Claim>();
+
+            for (int i = 0; i < roles.Count; i++)
+            {
+                roleClaims.Add(new Claim("roles", roles[i]));
+            }
+
+            string ipAddress = IpHelper.GetIpAddress();
+
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.UserName),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(JwtRegisteredClaimNames.Email, user.Email),
+                new Claim("uid", user.Id),
+                new Claim("ip", ipAddress)
+            }
+            .Union(userClaims)
+            .Union(roleClaims);
+
+            var symmetricSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtSettings.Key));
+            var signingCredentials = new SigningCredentials(symmetricSecurityKey, SecurityAlgorithms.HmacSha256);
+
+            var jwtSecurityToken = new JwtSecurityToken(
+                issuer: _jwtSettings.Issuer,
+                audience: _jwtSettings.Audience,
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(_jwtSettings.DurationInMinutes),
+                signingCredentials: signingCredentials);
+            return jwtSecurityToken;
+        }
+        private async Task<bool> SendVerificationEmail(AppUser user, string origin)
+        {
+            var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+            code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+            var route = "api/account/confirm-email/";
+            var _enpointUri = new Uri(string.Concat($"{origin}/", route));
+            var verificationUri = QueryHelpers.AddQueryString(_enpointUri.ToString(), "userId", user.Id);
+            verificationUri = QueryHelpers.AddQueryString(verificationUri, "code", code);
+            //Email Service Call Here
+            var mailRequest = new EmailRequest()
+            {
+                Subject = "Confirm Your Registration",
+                Body = $"Click {verificationUri} to complete your registration ",
+                ToEmail = user.Email
+            };
+
+            return await _emailService.SendEmailAsync(mailRequest);
+        }
+
+
+    }
+}
---
+++ SignalRAPI.Core/Interfaces/IAuthenticationService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalRAPI.Core.Interfaces
+{
+    public interface IAuthenticationService
+    {
+    }
+}
